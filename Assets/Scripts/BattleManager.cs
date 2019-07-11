2f40296a2fc035a29115e6d656193207b358093e
dyshields <dyshields@gmail.com>
2019-07-11 07:22:22 -0400

    Battle System updates
    Animations, effects, turn-based behavior

--- Assets/Scripts/BattleManager.cs
+++ Assets/Scripts/BattleManager.cs
@@ -14,6 +14,11 @@ public class BattleManager : MonoBehaviour
     public BattleChars[] playerPrefabs;
     public BattleChars[] enemyPrefabs;
     public List<BattleChars> activeBattlers = new List<BattleChars>();
+    public int currentTurn;
+    public bool turnWaiting;
+    public GameObject uiButtonsHolder;
+    public BattleMove[] movesList;
+    public GameObject enemyAttackEffect;
 
     void Start()
     {
@@ -27,6 +32,27 @@ public class BattleManager : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.P))
         {
             BattleStart(new string[] {"Eyeball", "Skeleton", "Spider", "Wizard" });
+        }
+
+        if(battleActive)
+        {
+            if(turnWaiting)
+            {
+                if(activeBattlers[currentTurn].isPlayer)
+                {
+                    uiButtonsHolder.SetActive(true);
+                } else
+                {
+                    uiButtonsHolder.SetActive(false);
+                    // enemy should attack
+                    StartCoroutine(EnemyMoveCo());
+                }
+            }
+
+            if (Input.GetKeyDown(KeyCode.O))
+            {
+                NextTurn();
+            }
         }
     }
 
@@ -83,6 +109,119 @@ public class BattleManager : MonoBehaviour
                     }
                 }
             }
+            turnWaiting = true;
+            // Randomizes which character goes first in battle; Count used for a list, Length for array
+            currentTurn = Random.Range (0, activeBattlers.Count);
         }
+    }
+
+    public void NextTurn()
+    {
+        currentTurn++;
+        if(currentTurn >= activeBattlers.Count)
+        {
+            currentTurn = 0;
+        }
+
+        turnWaiting = true;
+        UpdateBattle();
+    }
+
+    public void UpdateBattle()
+    {
+        bool allEnemiesDead = true;
+        bool allPlayersDead = true;
+
+        for(int i=0; i < activeBattlers.Count; i++)
+        {
+            if(activeBattlers[i].currentHP < 0)
+            {
+                activeBattlers[i].currentHP = 0;
+            }
+
+            if(activeBattlers[i].currentHP == 0)
+            {
+                // Handle dead character
+            } else
+            {
+                if(activeBattlers[i].isPlayer)
+                {
+                    allPlayersDead = false;
+                }
+                else
+                {
+                    allEnemiesDead = false;
+                }
+            }
+        }
+
+        if(allEnemiesDead || allPlayersDead)
+        {
+            if(allEnemiesDead)
+            {
+                // End battle in victory :)
+            } else
+            {
+                // End battle in defeat :(
+            }
+            battleScene.SetActive(false);
+            GameManager.instance.battleActive =false;
+            battleActive = false;
+        }
+    }
+
+    public IEnumerator EnemyMoveCo()
+    {
+        turnWaiting = false;
+        yield return new WaitForSeconds(1f);
+        EnemyAttack();
+        yield return new WaitForSeconds(1f);
+        NextTurn();
+
+    }
+
+    public void EnemyAttack()
+    {
+        // Code establishes an empty list of players, loops through to find them, ensures that they have more than 0 health,
+        // adds them to the list, selects one at random, and attacks
+        List<int> players = new List<int>();
+        for(int i=0; i < activeBattlers.Count; i++)
+        {
+            if(activeBattlers[i].isPlayer && activeBattlers[i].currentHP > 0)
+            {
+                players.Add(i);
+            }
+        }
+        int selectedTarget = players[Random.Range(0, players.Count)];
+
+        // activeBattlers[selectedTarget].currentHP -= 25;
+
+        int selectAttack = Random.Range(0, activeBattlers[currentTurn].movesAvailable.Length);
+        int movePower = 0;
+        for (int i = 0; i < movesList.Length; i++)
+        {
+            if(movesList[i].moveName == activeBattlers[currentTurn].movesAvailable[selectAttack])
+            {
+                Instantiate(movesList[i].theEffect, activeBattlers[selectedTarget].transform.position, activeBattlers[selectedTarget].transform.rotation);
+                movePower = movesList[i].movePower;
+            }
+        }
+
+        Instantiate(enemyAttackEffect, activeBattlers[currentTurn].transform.position, activeBattlers[currentTurn].transform.rotation);
+
+        DealDamage(selectedTarget, movePower);
+    }
+
+    public void DealDamage(int target, int movePower)
+    {
+        float attackPower = activeBattlers[currentTurn].strength + activeBattlers[currentTurn].wpnPower;
+        float defensePower = activeBattlers[target].defense + activeBattlers[target].armorPower;
+
+        float damageCalc = (attackPower / defensePower) * movePower * Random.Range(.9f, 1.1f);
+        int damageToGive = Mathf.RoundToInt(damageCalc);
+
+        Debug.Log(activeBattlers[currentTurn].charName + " is dealing " + damageCalc + "(" + damageToGive + ") damage to " + activeBattlers[target].charName);
+
+        activeBattlers[target].currentHP -= damageToGive;
     }
 }
---
+++ Assets/Scripts/DestroyOverLifetime.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOverLifetime : MonoBehaviour
+{
+
+    public float lifetime;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        Destroy(gameObject, lifetime);
+    }
+}
